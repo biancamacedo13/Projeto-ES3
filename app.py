965ca0e1957e8ba8fc93aa15930442ad272a0e8c
Karoline Oliveira Degan <karol.degan@hotmail.com>
2024-10-31 14:07:38 -0300

    aaa

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = 'sua_chave_secreta'
+CORS(app)
 
 from routes import *
 
